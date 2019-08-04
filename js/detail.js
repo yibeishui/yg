d3258f3b6566230a39cacf7b5978c67f648bb462
yibeishui <1719692497@qq.com>
2019-08-04 18:35:20 +0800

    列表页样式与动画效果

--- js/detail.js
+++ js/detail.js
@@ -12,3 +12,68 @@
 //     let yy = {type,middletype, smalltype,img, price, youhui, tit, tit, titcon, turnover, pinlun, shorw, shop };
 //     arr1.push(yy);
 // })
+
+$(function () {
+
+    // 栏目划入
+    $(".shopping-guide").on("mouseenter", ".shopping-guide-item", function () {
+        $(this).css({
+            borderColor: "red",
+            borderBottom:"none"       
+        });
+
+        $(this).find(".shopping-guide-titicon").animate({
+            "top": "-26px"
+        }, 200);
+
+        $(this).find(".shopping-guide-list").css("display","block")
+    });
+
+    // 栏目划出
+    $(".shopping-guide").on("mouseleave", ".shopping-guide-item", function () {
+        $(this).css({
+            borderColor: "#cccccc",
+            borderBottom:"1px solid #cccccc"
+        });
+        $(this).find(".shopping-guide-titicon").animate({
+            "top": "0px"
+        }, 200);
+        $(this).find(".shopping-guide-list").css("display","none")
+    });
+
+    // 收藏划入动画
+    $(".detail-item").on("mouseenter", ".item-toCollection", function () {
+        $(this).children(".icon-span1").animate({
+            "top": "-27px"
+        });
+    });
+
+    // 收藏划出动画
+    $(".detail-item").on("mouseleave", ".item-toCollection", function () {
+        $(this).children(".icon-span1").animate({
+            "top": "0px"
+        });
+    });
+
+    // input 聚焦 兄弟元素显示
+    $("input").focus(function(){
+        $(this).siblings("button").css("display","block").parent().css("background","#ffffff")
+    })
+
+    // 监听页面滚动 栏目定位
+   
+ $(document).scroll(function(){
+    let sortHigt=$(".shopping-sort").offset().top;
+    if(sortHigt<=0){
+        $(".shopping-sort").css({
+            position:"flex",
+            top:"0px"
+        })
+    }
+ })
+    
+
+
+
+
+})
